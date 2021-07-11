63ae05626331b68444a0a6d8d2068afe81928864
jelly-boop <jellyjelly111@naver.com>
2021-07-11 22:57:42 +0900

    react+firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBOmberFpZ5Ps1vnegcH56Fs9pkRgIlFwY",
+    authDomain: "twitter-d5ebf.firebaseapp.com",
+    projectId: "twitter-d5ebf",
+    storageBucket: "twitter-d5ebf.appspot.com",
+    messagingSenderId: "733034193431",
+    appId: "1:733034193431:web:d87555d7c428d5a1b1f252"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
