c3922485d5dfe35cd0ef6e542f705be6d6442a79
jelly-boop <jellyjelly111@naver.com>
2021-07-18 13:47:57 +0900

    log out & redirect

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigation from "./Navigation";
 
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
       <Router>
+        {/* 💡 Navigation이 존재하려면 isLoggedIn이 true여야 함 */}
+        {isLoggedIn && <Navigation/> } 
         <Switch>
           {isLoggedIn ? (
             <>
               <Route exact path="/">
                 <Home />
               </Route>
+              <Route exact path="/profile">
+                <Profile />
+              </Route>
+ 
             </>
           ) : (
+            <>
             <Route exact path="/">
               <Auth />
             </Route>
+    
+             </>
           )}
         </Switch>
       </Router>
