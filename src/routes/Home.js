43d8c3cf403b042ec6cd09305676041a6951b9b8
jelly-boop <70804578+jelly-boop@users.noreply.github.com>
2021-07-19 15:54:49 +0900

    preview img

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from "react";
 const Home = ({userObj}) => {
     const [tweet,setTweet]=useState("");
     const [tweets,setTweets]= useState([]);
-
+    const [attachment,setAttachment] = useState("");
     
     useEffect(()=>{
         // 💡 orderBy를 해야지 시간 순으로 뜬다!
@@ -17,7 +17,21 @@ const Home = ({userObj}) => {
            setTweets(tweetArray);
         })
     }, []);
-    
+    const onFileChange = (event) =>{
+        const {target : {files}}=event;
+        const theFile=files[0];
+        const reader =  new FileReader();
+        reader.onloadend= (finishedEvent) => {
+            const { currentTarget : { result }} = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile);
+    }
+    const onClearAttachmentClick = () => {
+        setAttachment(null);
+        document.getElementById("imgSrc").value="";
+    }
+
     const onSubmit = async (event)=>{
         event.preventDefault();
         await dbService.collection("tweets").add({
@@ -37,7 +51,14 @@ const Home = ({userObj}) => {
         <div>
         <form onSubmit={onSubmit}>
             <input type="text" value={tweet} onChange={onChange} placeholder="What's on your mind?" maxLength={120}/>
+            <input type="file" accept="image/*" id="imgSrc" onChange = {onFileChange}/>
             <input type="submit" value ="tweet"/>
+            {attachment && (
+                <div>
+                    <img src={attachment} width="50px" height="50px"/>
+                    <button onClick={onClearAttachmentClick} >Clear</button>
+                </div>
+            )}
         </form>
         <div >
             {tweets.map(tweet => 
